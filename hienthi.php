ae25ed2a35f2fc9c7554ac4bbb8b20535e51482f
tienhanh2001 <74296064+tienhanh2001@users.noreply.github.com>
2021-01-19 16:44:02 +0700

    Update hienthi.php

--- hienthi.php
+++ hienthi.php
@@ -124,9 +124,9 @@
 
 
 <?php
-$hostname = "localhost";
-$user = "root";
-$pass = "";
+$hostname = "34.200.32.20";
+$user = "Hanhnt";
+$pass = "123@123a";
 $db = "webzing";
 $con = mysqli_connect($hostname,$user,$pass,$db);
 mysqli_query($con,$db);

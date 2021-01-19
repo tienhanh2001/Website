180b5975408ad9b97aec19811b9ea85a1a49c215
tienhanh2001 <74296064+tienhanh2001@users.noreply.github.com>
2021-01-19 16:42:01 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -135,9 +135,9 @@
       <?php
          if(isset($_POST['delete']))
          {
-            $hostname = "localhost";
-            $user = "root";
-            $pass = "";
+            $hostname = "34.200.32.20";
+            $user = "Hanhnt";
+            $pass = "123@123a";
             $db = "webzing";
             $con = mysqli_connect($hostname,$user,$pass,$db);
             mysqli_query($con,$db);
